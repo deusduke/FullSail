f4d647ea46b90c98c4cd49e605e5584a9376e730
Deus Duke <deus@deusenterprises.com>
2014-03-19 13:19:18 -0700

    Initial commit for week 03 MDF2

---
+++ 1403_MDF2/MDF2 Project 03/MDF2 Project 03/MDFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDFAppDelegate.h
+//  MDF2 Project 03
+//
+//  Created by Deus Duke on 3/19/14.
+//  Copyright (c) 2014 DEUS Enterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 1403_MDF2/MDF2 Project 03/MDF2 Project 03/MDFMainViewController.m
@@ -0,0 +1,29 @@
+//
+//  MDFViewController.m
+//  MDF2 Project 03
+//
+//  Created by Deus Duke on 3/19/14.
+//  Copyright (c) 2014 DEUS Enterprises. All rights reserved.
+//
+
+#import "MDFMainViewController.h"
+
+@interface MDFMainViewController ()
+
+@end
+
+@implementation MDFMainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
